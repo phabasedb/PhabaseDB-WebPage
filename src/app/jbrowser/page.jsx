ad798000d5a80532851e3267ec5645f8e44ecd59
Pxcheco <132029761+Pxcheco@users.noreply.github.com>
2025-05-13 12:00:07 -0600

    jbrowse new style

--- src/app/jbrowser/page.jsx
+++ src/app/jbrowser/page.jsx
@@ -27,53 +27,58 @@ export default function JBrowsePage() {
         sx={{
           width: "90%",
           backgroundColor: "white",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          overflow: "hidden",
           borderRadius: 2,
           boxShadow: 5,
-          p: 1,
         }}
       >
-        <Typography
-          sx={{
-            fontWeight: 600,
-            fontSize: {
-              xs: "1.3rem",
-              sm: "1.5rem",
-              md: "1.7rem",
-              lg: "1.9rem",
-              xl: "2.1rem",
-            },
-          }}
-        >
-          Datasets available
-        </Typography>
-        <List>
-          {datasets.map((dataset) => (
-            <ListItem
-              key={dataset._id}
-              disablePadding
-              sx={{ cursor: "pointer" }}
-              onClick={() => handleDatasetClick(dataset)}
-            >
-              <ListItemText
-                primary={dataset.organism}
-                sx={{
-                  color: "blue",
-                  textDecoration: "underline",
-                  cursor: "pointer",
-                }}
-                primaryTypographyProps={{
-                  fontSize: {
-                    xs: "0.9rem",
-                    sm: "1.1rem",
-                    md: "1.2rem",
-                    lg: "1.4rem",
-                    xl: "1.6rem",
-                  },
-                }}
-              />
-            </ListItem>
-          ))}
-        </List>
+        <Box sx={{ width: "90%", my: 1 }}>
+          <Typography
+            sx={{
+              fontWeight: 600,
+              fontSize: {
+                xs: "1.3rem",
+                sm: "1.5rem",
+                md: "1.7rem",
+                lg: "1.9rem",
+                xl: "2.1rem",
+              },
+            }}
+          >
+            Datasets available
+          </Typography>
+          <List>
+            {datasets.map((dataset) => (
+              <ListItem
+                key={dataset._id}
+                disablePadding
+                sx={{ cursor: "pointer" }}
+                onClick={() => handleDatasetClick(dataset)}
+              >
+                <ListItemText
+                  primary={dataset.organism}
+                  sx={{
+                    color: "blue",
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                  }}
+                  primaryTypographyProps={{
+                    fontSize: {
+                      xs: "0.9rem",
+                      sm: "1.1rem",
+                      md: "1.2rem",
+                      lg: "1.4rem",
+                      xl: "1.6rem",
+                    },
+                  }}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
