976aa71aa940a3f369e963e261513f567c72251d
Pxcheco <132029761+Pxcheco@users.noreply.github.com>
2025-06-03 18:50:36 -0600

    struct card change resolution

--- src/app/home/card/struct-card.jsx
+++ src/app/home/card/struct-card.jsx
@@ -48,7 +48,7 @@ export default function StructCard({ item, index }) {
             alt="Informative image"
             sx={{
               width: "100%",
-              height: "100%",
+              height: "auto",
             }}
           />
         </Box>
