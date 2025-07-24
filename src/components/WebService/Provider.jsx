afbd5e14d6cf253c8e927e496d12dd8c5de13abd
Pxcheco <132029761+Pxcheco@users.noreply.github.com>
2025-07-24 15:07:46 -0600

    update provider.jsx in cache memory in apolloclient

--- src/components/WebService/Provider.jsx
+++ src/components/WebService/Provider.jsx
@@ -6,12 +6,26 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // local
-
 const URI_GRAPHQL = process.env.NEXT_PUBLIC_URI_GRAPHQL;
 
 const client = new ApolloClient({
   uri: `${URI_GRAPHQL}`,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      GeneInfo: {
+        keyFields: ["accessionId"],
+      },
+      Gene: {
+        fields: {
+          gene: {
+            merge(existing = {}, incoming) {
+              return { ...existing, ...incoming };
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default function WebServiceProvider({ children }) {
